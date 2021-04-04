1aa68d417b12b644a363cfbd78261b006ebeab94
913AliceHincu <53339016+913AliceHincu@users.noreply.github.com>
2021-04-04 16:07:57 +0300

    .

---
+++ cmake-build-debug/CMakeFiles/a67_913AliceHincu.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a67_913AliceHincu.dir/main.cpp.o"
+  "a67_913AliceHincu"
+  "a67_913AliceHincu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a67_913AliceHincu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
